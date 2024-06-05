5cc6a03adb94b851f2fdc73cabc53b7a1eb1380e
Valter Neto <valter.neto32@gmail.com>
2024-06-05 02:54:07 -0300

    chore(deps): adicionando o types do supertest. configurações de migrations para teste

--- tests/jest.setup.ts
+++ tests/jest.setup.ts
@@ -1,4 +1,14 @@
-import supertest from 'supertest'
+import supertest from 'supertest';
 import { app } from '../src/app'
+import { Knex } from '../src/database';
+
+beforeAll(async () => {
+    await Knex.migrate.latest();
+    await Knex.seed.run();
+  });
+  
+  afterAll(async () => {
+    await Knex.destroy();
+  });
 
 export const testServer = supertest(app)
